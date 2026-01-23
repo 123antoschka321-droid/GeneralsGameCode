e1e35df6e872523b61f6245973c39734fe7299fc
123antoschka321-droid <123antoschka321@gmail.com>
2026-01-23 03:23:01 +0300

    Update ControlBar.h

--- GeneralsMD/Code/GameEngine/Include/GameClient/ControlBar.h
+++ GeneralsMD/Code/GameEngine/Include/GameClient/ControlBar.h
@@ -409,7 +409,7 @@ enum {
 			};
 enum { MAX_STRUCTURE_INVENTORY_BUTTONS = 10 }; // there are this many physical buttons in "inventory" windows for structures
 enum { MAX_BUILD_QUEUE_BUTTONS = 9 };// physical button count for the build queue
-enum { MAX_SPECIAL_POWER_SHORTCUTS = 33};
+enum { MAX_SPECIAL_POWER_SHORTCUTS = 11};
 class CommandSet : public Overridable
 {
 
